851ec9bab5af1cce9ef128d80bd14699464e502c
Navzz876 <navaneets@gmail.com>
2023-05-13 17:45:21 +0530

    Setup DB connection to application

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

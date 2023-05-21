e08f491818b3efc028cfa69dd7b1ca224d16883e
Navzz876 <navaneets@gmail.com>
2023-05-21 11:30:28 +0530

    Added DB data in application and bootswatch themes

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        protected readonly ApplicationContext _applicationContext;
+        public CategoryController(ApplicationContext applicationContext) 
+        {
+            _applicationContext = applicationContext;
+        }
+        public IActionResult Index()
+        {
+            var categoryList= _applicationContext.Categories.ToList();
+            return View(categoryList);
+        }
+    }
+}
--- Data/ApplicationContext.cs
+++ Data/ApplicationContext.cs
@@ -10,5 +10,16 @@ namespace BookStore.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Drama", DisplayOrder=1},
+                 new Category { Id = 2, Name = "Horror", DisplayOrder = 2 },
+                  new Category { Id = 3, Name = "Sci-fi", DisplayOrder = 3 },
+                   new Category { Id = 4, Name = "Fiction", DisplayOrder = 4 },
+                    new Category { Id = 5, Name = "Comics", DisplayOrder = 5 }
+
+                );
+        }
     }
 }

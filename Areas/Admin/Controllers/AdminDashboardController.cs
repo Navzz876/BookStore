be344c0d5eb887e6cd42cc90618f4b9b16deb4cf
Navzz876 <navaneets@gmail.com>
2023-10-02 14:11:51 +0530

    Added Dashboard to Admin

---
+++ Areas/Admin/Controllers/AdminDashboardController.cs
@@ -0,0 +1,26 @@
+﻿using BookStore.Controllers;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models.Models;
+using BookStore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminDashboardController : Controller
+    {
+        private readonly ILogger<AdminDashboardController> _logger;
+        private readonly IProductRepository _productRepository;
+        public AdminDashboardController(ILogger<AdminDashboardController> logger, IProductRepository productRepository)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }     
+    }
+}

81bd1de2a3d356bc7284feb178616c59f752d8ae
Navzz876 <navaneets@gmail.com>
2023-10-15 14:55:56 +0530

    Added OrderSummary and BillingDetails

--- Areas/Home/Controllers/CartController.cs
+++ Areas/Home/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models.Models;
 using BookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -15,11 +16,13 @@ namespace BookStore.Areas.Home.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IProductRepository _productRepository;
         private readonly IShoppingCartRepository _shoppingCartRepository;
-        public CartController(ILogger<HomeController> logger, IProductRepository productRepository, IShoppingCartRepository shoppingCartRepository)
+        private readonly IApplicationUserRepository _applicationUserRepository;
+        public CartController(ILogger<HomeController> logger, IProductRepository productRepository, IShoppingCartRepository shoppingCartRepository, IApplicationUserRepository applicationUserRepository)
         {
             _logger = logger;
             _productRepository = productRepository;
             _shoppingCartRepository = shoppingCartRepository;
+            _applicationUserRepository = applicationUserRepository;
         }
         public IActionResult Index()
         {
@@ -35,6 +38,27 @@ namespace BookStore.Areas.Home.Controllers
                 shoppingCartViewModel.OrderTotal += price;
             }
             return View(shoppingCartViewModel);          
+        }
+        [HttpPost]
+        public IActionResult Index(ShoppingCartViewModel shoppingCartViewModel)
+        {
+               var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+                 shoppingCartViewModel = new ShoppingCartViewModel()
+                {
+                    ShoppingCartList = _shoppingCartRepository.GetAll(x => x.ApplicationUserId == userId, includeProperties: "Product"),
+                };
+                foreach (var shoppingCart in shoppingCartViewModel.ShoppingCartList)
+                {
+                    var price = shoppingCart.Product.Price * shoppingCart.Count;
+                    shoppingCartViewModel.OrderTotal += price;
+                }           
+            if(shoppingCartViewModel != null && shoppingCartViewModel.OrderTotal > 0)
+            {
+                return RedirectToAction("CheckOutForm", "Cart",shoppingCartViewModel);
+            }
+            TempData["failure"] = "Unable to check out at the moment, Please try again later";
+            return View(shoppingCartViewModel);
         }
         [HttpPost]
         public IActionResult Plus(int cartId)
@@ -70,6 +94,27 @@ namespace BookStore.Areas.Home.Controllers
             _shoppingCartRepository.Delete(shoppingCart);
             _shoppingCartRepository.Save();
             return RedirectToAction("Index");
+        }
+        public IActionResult CheckOutForm(ShoppingCartViewModel shoppingCartViewModel)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCartViewModel.ShoppingCartList = _shoppingCartRepository.GetAll(x => x.ApplicationUserId == userId, includeProperties: "Product");
+            shoppingCartViewModel.TotalCount = shoppingCartViewModel.ShoppingCartList.Count();
+            var appUserDetails = _applicationUserRepository.Get(x => x.Id == userId, includeProperties: null);
+            var billingDetails = new BillingDetails()
+            {
+                Name= appUserDetails.Name,
+                EmailAddress= appUserDetails.Email,
+                State= appUserDetails.State,
+                Address= appUserDetails.StreetAddress,
+                Address2= appUserDetails.StreetAddress,
+                ZipCode= appUserDetails.PostalCode,
+                City= appUserDetails.City,
+
+            };
+            shoppingCartViewModel.BillingDetails= billingDetails;
+            return View(shoppingCartViewModel);
         }
     }
 }
